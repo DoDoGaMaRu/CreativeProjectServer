5143048906f199da34fac7c8aa24dfd3bcef19d3
ilohas0021 <ilohas0021@naver.com>
2023-05-23 17:11:16 +0900

    first_commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,6 @@
 import updater.Updater;
 
 public class main {
-    static class CustomInteger {
-        int value;
-        public CustomInteger(int value) {
-            this.value = value;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         Updater updater = new Updater();
         updater.run();
